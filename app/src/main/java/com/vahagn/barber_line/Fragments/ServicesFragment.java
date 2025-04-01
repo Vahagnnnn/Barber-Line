fa6f6da2bfc29950e5c4e394429327443b271003
Vahagnnnn <vahagn.makaryan.v@gmail.com>
2025-04-01 10:23:10 +0400

    V-93(Edit and Delete Services Done)

--- app/src/main/java/com/vahagn/barber_line/Fragments/ServicesFragment.java
+++ app/src/main/java/com/vahagn/barber_line/Fragments/ServicesFragment.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -39,8 +40,8 @@ public class ServicesFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_services, container, false);
         servicesContainer = view.findViewById(R.id.services_container);
         if (serviceslists != null) {
-                for (Services services : serviceslists) {
-                addServices(servicesContainer, services);
+            for (Services services : serviceslists) {
+                addServices(services);
             }
         }
         return view;
@@ -51,16 +52,16 @@ public class ServicesFragment extends Fragment {
         servicesContainer.removeAllViews();
         if (serviceslists != null) {
             for (Services services : serviceslists) {
-                addServices(servicesContainer, services);
+                addServices(services);
             }
         }
     }
 
-    private void addServices(LinearLayout container, Services services) {
-        View servicesView = LayoutInflater.from(getContext()).inflate(R.layout.services, container, false);
+    private void addServices(Services services) {
+        View servicesView = LayoutInflater.from(getContext()).inflate(R.layout.services, servicesContainer, false);
         TextView serviceName = servicesView.findViewById(R.id.name);
-        TextView serviceDuration= servicesView.findViewById(R.id.duration);
-        TextView servicePrice= servicesView.findViewById(R.id.price);
+        TextView serviceDuration = servicesView.findViewById(R.id.duration);
+        TextView servicePrice = servicesView.findViewById(R.id.price);
 
         serviceName.setText(services.getName());
         serviceDuration.setText(services.getDuration());
@@ -70,53 +71,116 @@ public class ServicesFragment extends Fragment {
             Toast.makeText(getContext(), services.getName(), Toast.LENGTH_SHORT).show();
         });
 
-//        servicesView.setOnLongClickListener(v -> {
-//            showEditDeleteDialog(services);
-//            return true;
-//        });
+        servicesView.setOnLongClickListener(v -> {
+            showEditDeleteDialog(services);
+            return true;
+        });
 
-        container.addView(servicesView);
+        servicesContainer.addView(servicesView);
     }
 
-//    private void showEditDeleteDialog(Services services) {
+    private void showEditDeleteDialog(Services services) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Edit/Delete")
+                .setMessage("What do you want to do?")
+                .setPositiveButton("Edit", (dialog, which) -> editServices(services))
+                .setNegativeButton("Delete", (dialog, which) -> deleteServices(services))
+                .setNeutralButton("Cancel", (dialog, which) -> dialog.dismiss())
+                .show();
+    }
+
+//    private void editServices(Services services) {
 //        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-//        builder.setTitle("Edit/Delete")
-//                .setMessage("What do you want to do?")
-//                .setPositiveButton("Edit", (dialog, which) -> editSpecialist(services))
-//                .setNegativeButton("Delete", (dialog, which) -> deleteSpecialist(services))
-//                .setNeutralButton("Cancel", (dialog, which) -> dialog.dismiss())
-//                .show();
-//    }
-
-//    private void editSpecialist(Services services) {
-//        Toast.makeText(getContext(), "Editing " + specialist.getName(), Toast.LENGTH_SHORT).show();
-//        Intent intent = new Intent(getContext(), AddBarbersActivity.class);
-//        Edit = true;
 //
-//        AddBarbersActivity.name = specialist.getName();
-////        AddBarbersActivity.phoneNumber = specialist.getPhone();    private String name;
-////    private String price;
-////    public String duration;
-//        AddBarbersActivity.ListServiceEdit = specialist.getServices();
+//        EditText Service_Name_input = new EditText(getContext());
+//        Service_Name_input.setHint("Enter the name of the service");
+//        Service_Name_input.setText(services.getName());
 //
-//        startActivity(intent);
+//        EditText Service_Price_input = new EditText(getContext());
+//        Service_Price_input.setHint("Enter the price of the service");
+//        Service_Price_input.setText(services.getPrice());
+//
+//        EditText Service_Duration_input = new EditText(getContext());
+//        Service_Duration_input.setHint("Enter the duration of the service");
+//        Service_Duration_input.setText(services.getDuration());
+//
+//
+//        builder.setTitle("Edit Service")
+//                .setView(Service_Name_input)
+//                .setView(Service_Price_input)
+//                .setView(Service_Duration_input)
+//                .setPositiveButton("Save", (dialog, which) -> {
+//                    String newName = Service_Name_input.getText().toString();
+//                    String newPrice = Service_Price_input.getText().toString();
+//                    String newDuration = Service_Price_input.getText().toString();
+//
+//                    services.setName(newName);
+//                    services.setPrice(newPrice);
+//                    services.setDuration(newDuration);
+//
+//                    displayServices();
+//
+//                    Toast.makeText(getContext(), "Service updated", Toast.LENGTH_SHORT).show();
+//                })
+//                .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss());
+//
+//        builder.show();
 //    }
 
-//    private void deleteSpecialist(Services services) {
-//        serviceslists.remove(services);
-//        displayServices();
-//        AddBarbersActivity.imageUrl = null;
-//        AddBarbersActivity.name = null;
-////        AddBarbersActivity.phoneNumber = null;
-//        AddBarbersActivity.ListServiceEdit = null;
-//        Toast.makeText(getContext(), "Deleted " + services.getName(), Toast.LENGTH_SHORT).show();
-//    }
 
-//    private void displayServices() {
-//        container.removeAllViews();
-//        for (Services services : serviceslists) {
-//            addSpecialist(specialist);
-//        }
-//    }
+    private void editServices(Services services) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
 
+        LinearLayout layout = new LinearLayout(getContext());
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        EditText Service_Name_input = new EditText(getContext());
+        Service_Name_input.setHint("Enter the name of the service");
+        Service_Name_input.setText(services.getName());
+
+        EditText Service_Price_input = new EditText(getContext());
+        Service_Price_input.setHint("Enter the price of the service");
+        Service_Price_input.setText(services.getPrice());
+
+        EditText Service_Duration_input = new EditText(getContext());
+        Service_Duration_input.setHint("Enter the duration of the service");
+        Service_Duration_input.setText(services.getDuration());
+
+        layout.addView(Service_Name_input);
+        layout.addView(Service_Price_input);
+        layout.addView(Service_Duration_input);
+
+        builder.setTitle("Edit Service")
+                .setView(layout)
+                .setPositiveButton("Save", (dialog, which) -> {
+                    String newName = Service_Name_input.getText().toString();
+                    String newPrice = Service_Price_input.getText().toString();
+                    String newDuration = Service_Duration_input.getText().toString();
+
+                    services.setName(newName);
+                    services.setPrice(newPrice);
+                    services.setDuration(newDuration);
+
+                    displayServices();
+
+                    Toast.makeText(getContext(), "Service updated", Toast.LENGTH_SHORT).show();
+                })
+                .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss());
+
+        builder.show();
+    }
+
+
+    private void deleteServices(Services services) {
+        serviceslists.remove(services);
+        displayServices();
+        Toast.makeText(getContext(), "Deleted " + services.getName(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void displayServices() {
+        servicesContainer.removeAllViews();
+        for (Services services : serviceslists) {
+            addServices(services);
+        }
+    }
 }
